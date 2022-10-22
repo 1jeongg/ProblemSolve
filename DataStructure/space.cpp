02d1159936b860af81f09088116ea9e5fbe5d096
Nam WoeunJung <84652886+1jeongg@users.noreply.github.com>
2022-10-22 15:14:25 +0900

    [자료구조] 14번과제 - 우주청소(Refactor)
    수행시간 0.70

--- DataStructure/space.cpp
+++ DataStructure/space.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
+#include <tuple>
 using namespace std;
 
 void space(){
     int start_num, end_num, index;
-    vector<pair<int,int>> space;
-    vector<bool> mybool;
+    vector<tuple<int, int, bool>> space;
 
     cin >> start_num >> end_num;
     index = start_num % 3;
     for (int i=start_num; i<=end_num; i++){
-        space.push_back(make_pair(i, index));
-        mybool.push_back(false);
+        space.push_back(make_tuple(i, index, false));
         index++;
         if (index == 3) index = 0;
     }
@@ -21,8 +18,8 @@ void space(){
     index = 0;
 
     for (int i=0; i<end_num-start_num; i++){
-        mybool[index] = true;
-        int cnt = 1, my_max = space[index].second + 2, myindex = index;
+        get<2>(space[index]) = true;
+        int cnt = 1, my_max = get<1>(space[index]) + 2, myindex = index;
         while (cnt <= my_max){
             myindex++;
             if (myindex > end_num-start_num){
@@ -31,25 +28,12 @@ void space(){
                 cnt = 0;
             }
 
-            if(!mybool[index+cnt]) cnt++;
+            if(!get<2>(space[index+cnt])) cnt++;
             else index++;
         }
-
         index = myindex;
     }
-    cout << space[index].first << endl;
-
-    /*
-    for (int i=0; i<end_num-start_num; i++){
-        auto value = space.begin() + index;
-        index += (*value).second + 1;
-        if (index >= space.size()-1)
-            index = (*value).second + 2 - (space.end() - value);
-        if (space.size() <= 3) index = index%2;
-        space.erase(value);
-    }
-    */
-
+    cout << get<0>(space[index]) << endl;
 }
 int main(){
     space();
