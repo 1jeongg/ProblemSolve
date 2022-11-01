7dacef919801800c1c164273178f0a0731dca008
Nam WoeunJung <84652886+1jeongg@users.noreply.github.com>
2022-11-01 23:09:45 +0900

    [자료구조] 14번과제 - 우주청소(Refactor3)

--- DataStructure/space.cpp
+++ DataStructure/space.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <tuple>
 using namespace std;
-
 int start_num, end_num, start_id, end_id;
-vector<tuple<int, int, bool>> space;
+int moved, space_size, dist;
+vector<pair<int, bool>> space;
 
 void space_func(){
     cin >> start_num >> end_num;
     for (int i=start_num; i<=end_num; i++)
-        space.push_back(make_tuple(i, i%3, false));
+        space.push_back(make_pair(i, false));
 
-    for (int i=0; i<end_num-start_num; i++){
-        get<2>(space[start_id]) = true;
-        int moved = 1, distance = get<1>(space[start_id]) + 2;
-        while (moved <= distance){
+    space_size = end_num - start_num;
+    for (int i=0; i<space_size; i++){
+        space[start_id].second = true;
+        moved = 1;
+        dist = space[start_id].first%3 + 2;
+        while (moved <= dist){
             end_id += 1;
-            if (end_id > end_num-start_num){
-                distance -= moved;
+            if (end_id > space_size){
+                dist -= moved;
                 start_id = end_id = moved = 0;
             }
-            if(!get<2>(space[start_id+moved])) moved += 1;
+            if(!space[start_id+moved].second) moved += 1;
             else start_id += 1;
         }
         start_id = end_id;
     }
-    cout << get<0>(space[end_id]);
+    cout << space[end_id].first;
 }
 int main(){
     space_func();
