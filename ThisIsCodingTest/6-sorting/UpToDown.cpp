9696600bff72fe1608b6991fe2a2c1327e5b4506
Nam WoeunJung <84652886+1jeongg@users.noreply.github.com>
2023-02-16 11:48:27 +0900

    [예제 6-2/정렬/하] 위에서 아래로

---
+++ ThisIsCodingTest/6-sorting/UpToDown.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> sequence;
+
+void getInput() {
+    int sequence_size, inp;
+    cin >> sequence_size;
+    for (int i=0; i<sequence_size; i++) {
+        cin >> inp;
+        sequence.push_back(inp);
+    }
+}
+
+void sortByDescending() {
+    sort(sequence.begin(), sequence.end());
+    reverse(sequence.begin(), sequence.end());
+    for (auto s: sequence) cout << s << " ";
+}
+int main() {
+    getInput();
+    sortByDescending();
+    return 0;
+}
+
