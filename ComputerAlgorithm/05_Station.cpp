591477617743c616774b4f543c1a49af9691b352
Nam WoeunJung <84652886+1jeongg@users.noreply.github.com>
2023-03-25 15:45:12 +0900

    [알고리즘/분할정복/상] 우주 정거장
    아직까지는 찝찝하지만 네스파에서 통과는 됨..
    epsilon을 다르게 하면 틀리는 위험한 코드...

--- ComputerAlgorithm/05_Station.cpp
+++ ComputerAlgorithm/05_Station.cpp
@@ -2,14 +2,13 @@
 #include <vector>
 #include <tuple>
 #include <cmath>
-#include <cfloat>
-#include <algorithm>
 
 #define TUPLE tuple<double, double, double>
+#define EPS 0.00000001
 using namespace std;
 
 double a,b,c,d,e,f,x,y,z;
-double diff[2] = {0.5-FLT_EPSILON, 0.5+FLT_EPSILON};
+double diff[2] = {0.5-0.0001, 0.5+0.0001};
 
 double calculateDistance(TUPLE A, TUPLE B){
     tie(a,b,c) = A;
@@ -22,7 +21,7 @@ TUPLE getPoint(TUPLE A, TUPLE B, double k){
     return {a+k*(d-a), b+k*(e-b), c+k*(f-c)};
 }
 bool isEqual(double a, double b){
-    return abs(a-b) < FLT_EPSILON;
+    return fabs(a - b) < 0.00000001;
 }
 double getDistance(TUPLE A, TUPLE B, TUPLE C){
     TUPLE left = getPoint(B, C, diff[0]), right = getPoint(B,C, diff[1]);
@@ -44,8 +43,9 @@ double main_space(TUPLE A, TUPLE B, TUPLE C, TUPLE D){
         len[i+2] = getDistance(side[i+2], A, B);
     }
     if (isEqual(len[0], len[1])){
+        if (len[2] < len[3]) return main_space(A, B, C, side[3]);
         if (isEqual(len[2], len[3])) return len[2];
-        return main_space(C, D, A, B);
+        return main_space(A, B, side[2], D);
     }
     if (len[0] < len[1]) return main_space(A, side[1], C, D);
     return main_space(side[0], B, C, D);
@@ -57,7 +57,7 @@ void getInput() {
         points[i] = {x,y,z};
     }
     double answer = main_space(points[0], points[1], points[2], points[3]);
-    if(abs(answer - floor(answer)) > 0.3) answer++;
+    if(abs(answer - floor(answer)) > 0.001) answer++;
     cout << (int)answer;
 }
 int main() {
